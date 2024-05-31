f2bfa4573120ad759aeadf7aa58c0cf1498b3271
DIVAM SHARMA <divamsharma21@gmail.com>
2024-05-31 07:01:04 +0530

    sample program

---
+++ sample.java
@@ -0,0 +1,12 @@
+class sample{
+
+    public void addtwo(int a,int b){
+ int c=a+b;
+
+ System.out.println(c);
+    }
+    public static void main(String[] args) {
+        sample s=new sample();
+        s.addtwo(5,6);
+}
+}
