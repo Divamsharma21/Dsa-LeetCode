d14b89c46abf908baa4465cb5896cdafe8991c05
DIVAM SHARMA <103370650+Divamsharma21@users.noreply.github.com>
2024-06-10 08:24:35 +0530

    Time: 12 ms (9.40%), Space: 43.3 MB (68.27%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,18 +1,19 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+         Arrays.sort(nums); 
         List<List<Integer>> subset=new ArrayList<>();
-        ArrayList<Integer> ans=new ArrayList<>();
-             Arrays.sort(nums); 
-        dup(nums,subset,ans,0,nums.length);
+        // ArrayList<Integer> ans=new ArrayList<>();
+             
+        dup(nums,subset,new ArrayList<>(),0);
  
         return subset;
     }
     
-    public void dup(int[] nums, List<List<Integer>> subset,  ArrayList<Integer> ans,int idx,int n){
+    public void dup(int[] nums, List<List<Integer>> subset,  ArrayList<Integer> ans,int idx){
        
          if(subset.contains(ans)) return;
          
-        if(idx == n){
+        if(idx == nums.length){
             subset.add(new ArrayList<>(ans));
             return;
             
@@ -22,13 +23,13 @@ class Solution {
         
         // take
         
-        dup(nums,subset,ans,idx+1,n);
+        dup(nums,subset,ans,idx+1);
         // backtrack
          ans.remove(ans.size()-1);
         
         // not 
         
-        dup(nums,subset,ans,idx+1,n);
+        dup(nums,subset,ans,idx+1);
         
         
     }
